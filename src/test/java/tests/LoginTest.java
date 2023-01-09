496d8d43dc12140ef7ad5e84737afef33c4082ee
MichaelBlitshtein <michael.blitshtein@gmail.com>
2023-01-09 17:50:19 +0300

    Start

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class LoginTest extends TestBase{
+
+    @Test
+    public void loginSuccess(){
+
+    }
+
+    @Test
+    public void loginWrongEmail(){
+
+    }
+
+    @Test
+    public void loginWrongPassword(){
+
+    }
+
+    @Test
+    public void loginUnregisteredUser(){
+
+    }
+}
